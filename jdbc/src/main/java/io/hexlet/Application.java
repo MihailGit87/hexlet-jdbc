e1d781c933317f1f3b9dbe2f24f874612fb41bd0
mikhailvasilev <mall.87@mail.ru>
2023-11-20 14:15:45 +0300

    add preparedStatement and DELETE

--- jdbc/src/main/java/io/hexlet/Application.java
+++ jdbc/src/main/java/io/hexlet/Application.java
@@ -18,6 +18,23 @@ public class Application {
                 statement2.executeUpdate(sql2);
             }
 
+            var sql4 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql4)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "33333333");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.setString(2, "44444444");
+                preparedStatement.executeUpdate();
+            }
+
+            var sql5 = "DELETE FROM users WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(sql5)) {
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.executeUpdate();
+            }
+
             var sql3 = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
                 var resultSet = statement3.executeQuery(sql3);
